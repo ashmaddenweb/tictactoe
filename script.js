f10ebda9cf0aacd2288916423c839ea881b58943
ash-madden <ashleymadden55@gmail.com>
2022-06-30 02:20:59 +1000

    set up AI logic

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 let tile = document.querySelectorAll(".tile");
 let resetButton = document.getElementById("reset-button");
+const player = createPlayer();
+const opponent = createPlayer();
+const newGame = gameRules();
 
 // Creates a new player object 
 function createPlayer(playerName, playerChar) {
     return {
         playerName: playerName,
         playerChar: playerChar,
-        playerScore: [],
+        score: [],
         playerTries: 0,
         playerScoreDisplay() {
-            return this.playerScore;
+            return this.score;
         }
-
     };
 }
 
@@ -31,31 +33,103 @@ function gameRules() {
     }
 }
 
-const player = createPlayer();
-const newGame = gameRules();
-
 // Whichever tile the player clicks will display an "X" character
-for (let i = 0; i < tile.length; i++) {
-    tile[i].addEventListener("click", function() {
-        if (tile[i].innerHTML === "") {
-            player.playerScore.push(parseInt(tile[i].id));
-            player.playerTries++;
-            tile[i].className = "text-style";
-            tile[i].appendChild(document.createTextNode("X"));
-        }
-        if (player.playerTries > 2) {
-            for (let i = 0; i < newGame.win.length; i++) {
-                console.log(player.playerScore.sort(), "Score")
-                console.log(newGame.win[i]);
-                if (player.playerScore.includes(newGame.win[i][0]) && player.playerScore.includes(newGame.win[i][1]) && player.playerScore.includes(newGame.win[i][2])) {
-                    alert("Win");
+function startGame() {
+
+    // checks if space on board is taken or not for players move
+    for (let i = 0; i < tile.length; i++) {
+        tile[i].addEventListener("click", function() {
+            if (tile[i].innerHTML === "") {
+                player.score.push(parseInt(tile[i].id));
+                player.playerTries++;
+                tile[i].appendChild(document.createTextNode("X"));
+                return opponentTurn(newGame.win, opponent, player);
+            }
+
+            // Checks the squares selected by the player once they have had 3 turns and calculates if it's a win or not. 
+            if (player.playerTries > 2) {
+                for (let i = 0; i < newGame.win.length; i++) {
+                    if (player.score.includes(newGame.win[i][0]) && player.score.includes(newGame.win[i][1]) && player.score.includes(newGame.win[i][2])) {
+                        return alert("You Win!");
+                    }
                 }
             }
+        });
+    };
+}
+
+// Chooses opponents move based on situation 
+function opponentTurn(gameRules, opponent, player) {
+    opponent.playerTries++;
+
+    // Loops through all of the tiles on each instance of this function, to ensure none have been missed
+    for (let i = 0; i < tile.length; i++) {
+        if (tile[i].innerHTML === "") {
+
+            // Logic for the AI 
+            for (let j = 0; j < gameRules.length; j++) {
+                if (opponent.score.includes(gameRules[j][0]) && opponent.score.includes(gameRules[j][1])) {
+                    if (document.getElementById(gameRules[j][2]).innerHTML === "") {
+                        opponent.score.push(parseInt(gameRules[j][2]));
+                        document.getElementById(gameRules[j][2]).innerHTML = "O";
+                        return alert("Opponent Wins!");
+                    }
+                }
+                if (opponent.score.includes(gameRules[j][1]) && opponent.score.includes(gameRules[j][2])) {
+                    if (document.getElementById(gameRules[j][0]).innerHTML === "") {
+                        opponent.score.push(parseInt(gameRules[j][0]));
+                        document.getElementById(gameRules[j][0]).innerHTML = "O";
+                        return alert("Opponent Wins!");
+
+                    }
+                }
+                if (opponent.score.includes(gameRules[j][2]) && opponent.score.includes(gameRules[j][0])) {
+                    if (document.getElementById(gameRules[j][1]).innerHTML === "") {
+                        opponent.score.push(parseInt(gameRules[j][1]));
+                        document.getElementById(gameRules[j][1]).innerHTML = "O";
+                        return alert("Opponent Wins!");
+                    }
+                }
+            }
+
+            for (let k = 0; k < gameRules.length; k++) {
+                if (player.score.includes(gameRules[k][0]) && player.score.includes(gameRules[k][1])) {
+                    if (document.getElementById(gameRules[k][2]).innerHTML === "") {
+                        opponent.score.push(parseInt(gameRules[k][2]));
+                        return document.getElementById(gameRules[k][2]).innerHTML = "O";
+                    }
+                }
+                if (player.score.includes(gameRules[k][2]) && player.score.includes(gameRules[k][0])) {
+                    if (document.getElementById(gameRules[k][1]).innerHTML === "") {
+                        opponent.score.push(parseInt(gameRules[k][1]));
+                        return document.getElementById(gameRules[k][1]).innerHTML = "O";
+                    }
+                }
+                if (player.score.includes(gameRules[k][1]) && player.score.includes(gameRules[k][2])) {
+                    if (document.getElementById(gameRules[k][0]).innerHTML === "") {
+                        opponent.score.push(parseInt(gameRules[k][0]));
+                        return document.getElementById(gameRules[k][0]).innerHTML = "O";
+                    }
+                }
+            }
+        };
+    }
+
+    function randomMove() {
+        for (let i = 0; i < tile.length; i++) {
+            let num = Math.floor(Math.random() * 9) + 1;
+            if (document.getElementById(num).innerHTML === "") {
+                opponent.score.push(parseInt(num));
+                return document.getElementById(num).innerHTML = "O";
+            }
         }
-    });
+    }
+    randomMove();
 }
 
 // Reloads the page when you click the "Reset" button
 resetButton.addEventListener("click", function() {
     window.location.reload();
 });
+
+startGame();
